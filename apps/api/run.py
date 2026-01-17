af09982847f84b877ba7baef787b4ca1cc430f6d
Romeo <romeomeshack34@gmail.com>
2026-01-17 19:59:36 +0100

    Create all DB at server start context

--- apps/api/run.py
+++ apps/api/run.py
@@ -1,6 +1,12 @@
 from server import create_app
+from server.extensions import db
+
+# ----------------------------------
 
 server = create_app()
+
+with server.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     server.run(debug=True)
